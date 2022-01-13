6248235ca611021c3d346b23a975d82046d4c855
ShawnZou <shawnzou@zhejianglab.com>
2022-01-13 14:39:25 +0800

    build lstm model

---
+++ E2EDemodulationModel_LSTM.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 22 14:27:14 2021
+
+@author: ShawnZou
+"""
+import tensorflow as tf
+import random as biubiubiu
+import matplotlib.pyplot as plt
+import struct
+import time
+import os
+
---
+++ ReadMe.md
@@ -0,0 +1,1 @@
+Warnning: Currently no Licenses set for this project
